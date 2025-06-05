be4fc66b445b386f467210a90fbea27b9f356ad3
kevin <kevinyairgarciasalazar42@gmail.com>
2025-06-05 03:14:37 -0600

    correciondepediso

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using Google.Cloud.Firestore;
 
 namespace FirebaseApi.Models
 {
+    [FirestoreData]
     public class Pedido
     {
+        [FirestoreDocumentId]
         public string Id { get; set; } = string.Empty;
+
+        [FirestoreProperty]
         public string TipoPollo { get; set; } = string.Empty;
+
+        [FirestoreProperty]
         public string Acompañamientos { get; set; } = string.Empty;
+
+        [FirestoreProperty]
         public int Cantidad { get; set; }
+
+        [FirestoreProperty]
         public double PrecioTotal { get; set; }
+
+        [FirestoreProperty]
         public string UsuarioEmail { get; set; } = string.Empty;
+
+        [FirestoreProperty]
         public string Detalle { get; set; } = string.Empty;
     }
 }
