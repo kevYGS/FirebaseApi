3358b47a24199c6550c563d48e731a54ffcdef79
kevin <kevinyairgarciasalazar42@gmail.com>
2025-06-05 02:42:36 -0600

    correccionfirebaasetres

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,11 +1,15 @@
-﻿namespace FirebaseApi.Models
-public class Pedido
+﻿using System;
+
+namespace FirebaseApi.Models
 {
-    public string Id { get; set; } = string.Empty; 
-    public string TipoPollo { get; set; } = string.Empty;
-    public string Acompañamientos { get; set; } = string.Empty;
-    public int Cantidad { get; set; }
-    public double PrecioTotal { get; set; }
-    public string UsuarioEmail { get; set; } = string.Empty;
-    public string Detalle { get; set; } = string.Empty;
+    public class Pedido
+    {
+        public string Id { get; set; } = string.Empty;
+        public string TipoPollo { get; set; } = string.Empty;
+        public string Acompañamientos { get; set; } = string.Empty;
+        public int Cantidad { get; set; }
+        public double PrecioTotal { get; set; }
+        public string UsuarioEmail { get; set; } = string.Empty;
+        public string Detalle { get; set; } = string.Empty;
+    }
 }
